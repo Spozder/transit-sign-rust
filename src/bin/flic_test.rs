80e07122645560e8531a6db462db438be66f1eee
Spencer Pozder <spozder@panoramaed.com>
2025-03-30 22:35:17 -0400

    omg it was the wrong opcode the whole time

--- src/bin/flic_test.rs
+++ src/bin/flic_test.rs
@@ -8,7 +8,7 @@ use libc;
 
 // Constants from the Flic protocol
 const CMD_GET_INFO: u8 = 0x00;
-const EVT_GET_INFO_RESPONSE: u8 = 0x16;
+const EVT_GET_INFO_RESPONSE: u8 = 0x09;
 
 // Helper function to print bytes
 fn format_bytes(bytes: &[u8]) -> String {
